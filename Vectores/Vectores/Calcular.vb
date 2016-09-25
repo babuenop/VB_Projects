fafcf7bdd6b4b7b46605e632c9935a5dcf18a7ed
Alberto Bueno <abettetto@gmail.com>
2016-09-25 01:01:12 -0500

    Clase Calcular
    Implementacion de la clase calculadora para la seleccion de operaciones

---
+++ Vectores/Vectores/Calcular.vb
@@ -0,0 +1,30 @@
+﻿'
+' Created by SharpDevelop.
+' User: abueno
+' Date: 24/9/2016
+' Time: 22:35
+' 
+' To change this template use Tools | Options | Coding | Edit Standard Headers.
+'
+Public Class Calcular
+	Function Sumar (ByVal n1 As Integer, ByVal n2 As Integer)
+		Dim total As Integer
+		total= n1+n2
+		Return total
+	End Function
+	Function Restar(ByVal n1 As Integer, ByVal n2 As Integer)
+		Dim total As Integer
+		total= n1-n2
+		Return total
+	End Function
+	Function Multiplicar(ByVal n1 As Integer, ByVal n2 As Integer)
+		Dim total As Integer
+		total= n1*n2
+		Return total
+	End Function
+	Function Dividir(ByVal n1 As Integer, ByVal n2 As Integer)
+		Dim total As Integer
+		total= n1/n2
+		Return total
+	End Function
+End Class
